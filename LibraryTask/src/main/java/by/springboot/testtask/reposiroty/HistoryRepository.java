e88fef66b66c451600e7f9711df99e4b7d1c6bd0
ArtemGit <novitskii.a.o@gmail.com>
2017-05-21 15:50:10 +0300

    Репозиторий для истории, поиск добавление

--- LibraryTask/src/main/java/by/springboot/testtask/reposiroty/HistoryRepository.java
+++ LibraryTask/src/main/java/by/springboot/testtask/reposiroty/HistoryRepository.java
@@ -1,9 +1,9 @@
 package by.springboot.testtask.reposiroty;
 
 
-import by.springboot.testtask.domain.Book;
+import by.springboot.testtask.domain.History;
 import org.springframework.data.repository.CrudRepository;
 
-public interface HistoryRepository extends CrudRepository<Book, Integer > {
+public interface HistoryRepository extends CrudRepository<History, Integer > {
 
 }
